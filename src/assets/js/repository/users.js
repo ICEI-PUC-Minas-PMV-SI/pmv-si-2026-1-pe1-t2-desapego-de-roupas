e02998e93820286b8e09395ababf678a5fee34ac
Clayton Melo <claytonsnnt@gmail.com>
2026-05-05 17:10:42 -0300

    Added functions to get user

--- src/assets/js/repository/users.js
+++ src/assets/js/repository/users.js
@@ -1,5 +1,19 @@
 
 
+const STORAGE_KEY = "users";
+
+const CURRENT_USER = "current_user";
+
+
 export function getAll () {
-    var users = localStorage.getItem()
+    var users = localStorage.getItem(STORAGE_KEY);
+
+    if (users) {
+        return JSON.parse(users)
+    }
+    return []
+}
+
+export function findByEmail (email) {
+    return getAll().find(u => u.email === email)
 }
